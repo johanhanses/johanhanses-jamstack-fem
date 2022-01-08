abbadd4260b59a4a2514238b36a21016e85c0946
Johan Hanses <johanhanses@gmail.com>
2022-01-08 22:18:16 +0100

    fix login

--- src/components/route-login.js
+++ src/components/route-login.js
@@ -1,11 +1,19 @@
 import React from "react"
+import { useIdentityContext } from "react-netlify-identity"
+import { navigate } from "gatsby"
 
-export default function RouteLogin() {
+export default function RouteLogin({ showModal }) {
+  const identity = useIdentityContext()
+
+  if (identity && identity.isLoggedIn) {
+    navigate("/dashboard/secret", { replace: true })
+  }
+
   return (
     <>
       <h1> Log In or Sign Up </h1>
 
-      <button>Log In</button>
+      <button onClick={showModal}>Log In</button>
     </>
   )
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,18 +1,24 @@
 import { navigate } from "gatsby"
 import { Router } from "@reach/router"
-import React, { useEffect } from "react"
+import IdentityModal from "react-netlify-identity-widget"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import Profile from "../components/profile"
 import RouteBase from "../components/route-base"
 import RouteSecret from "../components/route-secret"
 import RouteLogin from "../components/route-login"
+import "react-netlify-identity-widget/styles.css"
 
 export default function Dashboard({ location }) {
+  const [isVisible, setIsVisible] = useState(false)
+
   useEffect(() => {
     if (location.pathname.match(/^\/dashboard\/?$/)) {
       navigate("/dashboard/login", { replace: true })
     }
   }, [location.pathname])
+
+  const showModal = () => setIsVisible(true)
 
   return (
     <Layout>
@@ -21,8 +27,13 @@ export default function Dashboard({ location }) {
       <Router>
         <RouteBase path="/dashboard/base" />
         <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
+        <RouteLogin path="/dashboard/login" showModal={showModal} />
       </Router>
+
+      <IdentityModal
+        showDialog={isVisible}
+        onCloseDialog={() => setIsVisible(false)}
+      />
     </Layout>
   )
 }
